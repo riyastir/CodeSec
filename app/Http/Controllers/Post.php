6e18eba88f51e577fbe77af7bccf7952cdd852ba
Mohamed Riyas <riyaskp1234@gmail.com>
2018-04-25 00:40:36 +0530

    post controller added

---
+++ app/Http/Controllers/Post.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cases;
+use DB;
+use Auth;
+class Post extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+	public function cases(Request $request){
+		$name = $request->input('name');
+		$gender = $request->input('gender');
+		$age = $request->input('age');
+		$missing_from = $request->input('missing_from');
+		$missing_date = $request->input('missing_date');
+		$district = $request->input('district');
+		$police_station = $request->input('police_station');
+		$height = $request->input('height');
+		$complexion = $request->input('complexion');
+		$scar = $request->input('scar');
+		$mole = $request->input('mole');
+		$tatto = $request->input('tatto');
+		$face = $request->input('face');
+		$lower_dress = $request->input('lower_dress');
+		$lower_dress_color = $request->input('lower_dress_color');
+		$upper_dress = $request->input('upper_dress');
+		$upper_dress_color = $request->input('upper_dress_color');
+		$photo = $request->input('photo');
+		$contact = $request->input('contact');
+		$notes = $request->input('notes');
+		
+		$case = new Cases();
+		$case->name = $name;
+		$case->gender = $gender;
+		$case->age = $age;
+		$case->missing_from = $missing_from;
+		$case->missing_date = $missing_date;
+		$case->district = $district;
+		$case->police_station = $police_station;
+		$case->height = $height;
+		$case->complexion = $complexion;
+		$case->scar = $scar;
+		$case->mole = $mole;
+		$case->tatto  = $tatto;
+		$case->face = $face;
+		$case->lower_dress = $lower_dress;
+		$case->upper_dress = $upper_dress;
+		$case->upper_dress_color = $upper_dress;
+		$case->lower_dress_color = $lower_dress_color;
+		$case->photo = $photo;
+		$case->contact_number = $contact_number;
+		$case->notes = $notes;
+		$case->created_by = $created_by;
+		$case->updated_by = $updated_by;
+		$case->created_at = $created_at;
+		$case->updated_at = $updated_at;
+		$case->save();
+		
+		$case_id = $case->id;
+		echo json_encode($case_id);
+	}
+}
